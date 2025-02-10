ffa81c5c54557ec621b8ff98a76e579620143301
Hassana207 <hassan.ayinde@dcmail.ca>
2025-02-10 13:14:33 -0500

    did a rough work at the momement

---
+++ Exercises/Array.java
@@ -0,0 +1,18 @@
+public class Array {
+    String [] names = {"Hassan", "Nadhir", "Trinity"};
+    int [] age = {20,20,19};
+
+    int [] grades = new  int [3];
+    String [] students = new String [3];
+
+    int count1 = names.length;
+    int count2 = age.length;
+
+    public static void passValue(int grades){
+        grades[0] = grade1;
+        grades [1] = grade2;
+        grades [2] = grade3;
+
+    }
+
+}

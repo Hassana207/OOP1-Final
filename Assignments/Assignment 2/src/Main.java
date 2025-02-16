b357d1f62ad64fa59dca4d1a37823a66fe05756a
Hassana207 <hassan.ayinde@dcmail.ca>
2025-02-16 18:06:19 -0500

    main java done for now

---
+++ Assignments/Assignment 2/src/Main.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        //initialising my variable for my input
+        int numberOfElement;
+        Scanner scanner = new Scanner (System.in);
+//while loop to implement number of arrays from user
+        while (true){
+            System.out.println("Please the the number of elements in an array (maximum 20).");
+               numberOfElement = scanner.nextInt();
+
+//if and else condition to check user input is not more than 20
+                if (numberOfElement > 20){
+                    System.out.println("please number of elements must not be more than 20");
+                }
+                else{
+                    System.out.println("Number of elements can not be more than 20 please ");
+                    break;
+                }
+        }
+
+        //initialising array size to user input
+        int [] array = new int [numberOfElement];
+
+        //for loop to collect and store array number of elements
+        for (int i=0;i<array.length;i++){
+            System.out.println("please enter arrays element");
+            int arrayElement =scanner.nextInt();
+            array[i] = arrayElement;
+
+        }
+
+    }
+}

8ca1d4b4f15e0f6079e8969a596cb210136bf992
Hassana207 <hassan.ayinde@dcmail.ca>
2025-03-08 16:50:05 -0500

    called findMax method to display maximum number in an array and called recursive method to display a strings in recursive order

--- Assignments/src/Main.java
+++ Assignments/src/Main.java
@@ -1,4 +1,6 @@
+
 import  java.util.Scanner ;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -15,18 +17,50 @@ public class Main {
         int secondValue = scanner.nextInt();
 
         // called calculateSum method and displayed result
-        int result = Methods.calculateSum(firstValue,secondValue);
-        System.out.println("sum:" + " " + result);
+        int sum = Methods.calculateSum(firstValue,secondValue);
+        System.out.println("sum:" + " " + sum);
 
-        // called multiply method that returns Product of two numbers been called and
-        // displayed output
-        int output = Methods.multiply(12,10);
-        System.out.println("Product of two numbers:" + " " + output);
+        // called multiply method that returns Product of two numbers and passed
+        // two argument then displayed result
+        int productOfTwo = Methods.multiply(12,10);
+        System.out.println("Product of two numbers:" + " " + productOfTwo);
 
         // called multiply method that returns Product of three numbers been called and
-        // displayed product
-        int product = Methods.multiply(15,12,11);
-        System.out.println("Product of three numbers: " + " " + product);
+        // passed three argument then displayed result
+        int productOfThree = Methods.multiply(15,12,11);
+        System.out.println("Product of three numbers: " + " " + productOfThree);
+
+        System.out.println("Enter size of Arrays:");
+        int numbers = scanner.nextInt();
+
+        //initialising array size
+        int [] array = new int[numbers];
+        System.out.println("Enter Arrays");
+        //for loop to collect and store array number of elements
+        for (int i=0;i<array.length;i++){
+            int arrayElement =scanner.nextInt();
+             array[i] = arrayElement;
         }
+
+        //called findMax method and displayed largestValue
+        int largestValue = Methods.findMax(array);
+        System.out.println("Maximum value:"+ " " + largestValue);
+
+
+        scanner.nextLine();
+
+
+         // prompting user to enter a string
+        System.out.println("Enter a string:");
+        String strings = scanner.nextLine();
+
+
+        // Called the reverseString method that return string in reverse order
+        // and passed strings and its length as argument and also minus string
+        // length minus by 1 because highest index is always length - 1
+        String reversedString = Methods.reverseString(strings, strings.length() - 1);
+        System.out.println("Reversed string: " + " " + reversedString);
+        }
+
 
 }
--- Assignments/src/Methods.java
+++ Assignments/src/Methods.java
@@ -1,6 +1,6 @@
 public class Methods {
 
-    // method to calculate the sum of two numbers
+    // method to return the sum of two numbers
     public static int calculateSum(int firstValue , int secondValue ){
         return  firstValue + secondValue ;
 
@@ -10,8 +10,46 @@ public class Methods {
     public  static int multiply (int firstValue , int secondValue){
         return firstValue * secondValue ;
     }
+
+
     // method to return multiplication of three values
     public  static int multiply (int firstValue , int secondValue, int thirdValue){
         return firstValue * secondValue * thirdValue ;
     }
+
+    //method to return the maximum value in an array
+    public static int findMax(int[] array) {
+        // Initialize maxValue to the first element of the array
+        int maxValue = array[0];
+
+        // Loop through my array to compare each element
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > maxValue) {
+                // Update maximum Value if a larger number is found
+                maxValue = array[i];
+            }
+        }
+        return maxValue;
+    }
+
+    // Recursive method to return string in reverse order
+    public static String reverseString(String strings, int index) {
+        // Base case: if index is less than 0
+        if (index < 0) {
+            // returning empty string as base case
+            return "";
+        }
+
+
+        // Returning the character at the current index using charAt method
+        // charAt methods return the value at the specified index
+        // then method recursively call itself with the next index (index - 1),
+        return strings.charAt(index) +  reverseString(strings, index - 1) ;
+
+
+    }
+
+
+
+
 }
